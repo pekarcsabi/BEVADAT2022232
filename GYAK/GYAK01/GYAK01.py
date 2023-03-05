99c2798dac8ae3d4dd2c1eae9be4658e0ac08b53
pekarcsabi <pekarcsabi@gmail.com>
2023-03-05 21:42:03 +0100

    done

--- GYAK/GYAK01/GYAK01.py
+++ GYAK/GYAK01/GYAK01.py
@@ -1,32 +1,31 @@
-numbers = [1, 3, 5, 7, 8]
+def  contains_odd(input_list):
+  
+  i=0
+  while((i<len(input_list)) and (not (input_list[i]%2!=0))):
+   i+=1
+  odd=(i<len(input_list))
+  return odd
 
-def contains_odd(input_list):
-    i = 0
-    while i < 5:
-        if (input_list[i] % 2 == 0):
-            return True
-        else:
-            False
-
-def is_odd(input_list: int) -> bool:
-    res_list = []
-    for index in input_list:
-        if (input_list[index - 1] % 2 == 0):
-            res_list.append(True)
-        else:
-            res_list.append(False)
-    return res_list
+def is_odd(input_list):
+ mask=[]
+ for index in range(len(input_list)):
+   if input_list[index]%2==0:
+    mask.append(False)
+   else:
+    mask.append(True)
+ return mask
 
 def element_wise_sum(input_list_1, input_list_2):
-    if len(input_list_1) != len(input_list_2):
-        raise ValueError("The two input lists must have the same length.")
-    result = []
-    for i in range(len(input_list_1)):
-        result.append(input_list_1[i] + input_list_2[i])
-    
-    return result
+    output_list=[]
+    for index1 in range(len(input_list_1)) :
+       output_list.append(input_list_1[index1]+input_list_2[index1])
+        
+           
+    return output_list
 
 def dict_to_list(input_dict):
-    result = list(input_dict.items())
-    
-    return result
+    list_help=[]
+    for key,value in input_dict.items():
+        list_help.append(f"({key},{value})")
+    _tuple=tuple(list_help)
+    return _tuple
