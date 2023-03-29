43d6b8deaec8db804f11770c267044d1b708e4ca
pekarcsabi <pekarcsabi@gmail.com>
2023-03-29 15:21:37 +0200

    asd

--- GYAK/GYAK05/GYAK05.py
+++ GYAK/GYAK05/GYAK05.py
@@ -52,6 +52,7 @@ class KNNClassifier:
     def confusion_matrix(self):
         conf_matrix = confusion_matrix(self.y_test, self.y_preds)
         sns.heatmap(conf_matrix,annot=True)
-
+    
+    @property
     def k_neihbors(self):
         return self.k
