ac056bee784384ca00fccb9da30e53acf7664601
pekarcsabi <pekarcsabi@gmail.com>
2023-04-03 21:08:46 +0200

    asd

--- GYAK/GYAK05/GYAK05.py
+++ GYAK/GYAK05/GYAK05.py
@@ -1,11 +1,10 @@
 import numpy as np
+import seaborn as sns
 from typing import Tuple
 from scipy.stats import mode
 from sklearn.metrics import confusion_matrix
-import seaborn as sns
 
 class KNNClassifier:
-    counter = 0
 
     def __init__(self, k:int, test_split_ratio :float):
         self.k = k
@@ -23,7 +22,6 @@ class KNNClassifier:
         x,y = dataset[:,:4],dataset[:,-1]
         return x,y
     
-    @classmethod
     def train_test_split(self, features:np.ndarray, labels:np.ndarray):
         test_size = int(len(features)* self.test_split_ratio)
         train_size = len(features) - test_size
@@ -32,11 +30,9 @@ class KNNClassifier:
         self.x_train,self.y_train = features[:train_size,:],labels[:train_size]
         self.x_test,self.y_test = features[train_size:train_size+test_size,:], labels[train_size:train_size + test_size]
     
-    @classmethod
     def euclidean(self, element_of_x:np.ndarray) -> np.ndarray:
         return np.sqrt(np.sum((self.x_train - element_of_x)**2, axis=1))
     
-    @classmethod
     def predict(self, x_test:np.ndarray):
         labels_pred = []
         for x_test_element in x_test:
@@ -47,12 +43,10 @@ class KNNClassifier:
         self.y_preds = np.array(labels_pred,dtype=np.int32)
         return self.y_preds
     
-    @classmethod
     def accuaracy(self) -> float:
         true_positive = (self.y_test == self.y_preds).sum()
         return true_positive / len(self.y_test) * 100
     
-    @classmethod
     def confusion_matrix(self) -> np.ndarray:
         return confusion_matrix(self.y_test,self.y_preds)
     
